7aa34ee78c213196aba5864f74d5ad00f959efc6
shinshin <shinshinlive@gmail.com>
2026-06-08 13:55:34 +0200

    Refactor App component to remove unused Button and update index.css to add new color variables and apply background color changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button'
+import * as ptd from "periodic-table-data";
+
+console.log(ptd.tableData);
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Button>shadcn/ui ready</Button>
-    </div>
-  )
+  return <div className="min-h-screen flex items-center justify-center"></div>;
 }
 
-export default App
+export default App;
